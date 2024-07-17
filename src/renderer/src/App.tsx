92176030a8d26b56eaeecf35cfe89867060ff309
islem-boudja <i_boudja@estin.dz>
2024-07-17 18:58:45 +0100

    feat: Update App component to use trpc for data fetching
    This commit updates the App component in the src/renderer/src/App.tsx file to use trpc for data fetching. It replaces the previous ipcHandle function with a trpcClient and queryClient setup. The trpc.Provider and QueryClientProvider components are added to wrap the Home component, enabling trpc functionality throughout the application. This change enhances the data fetching capabilities of the application by leveraging trpc's features.

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,35 +1,31 @@
-import Versions from './components/Versions'
-import electronLogo from './assets/electron.svg'
+import { useState } from 'react'
+import { trpc } from './util'
+import { ipcLink } from 'electron-trpc/renderer'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+// import {IpcRequest} from "../api";
+import Home from './Home'
+import { SuperJSON } from 'superjson'
+import { loggerLink } from '@trpc/client'
 
-function App(): JSX.Element {
-  const ipcHandle = (): void => window.electron.ipcRenderer.send('ping')
-
+export default function App() {
+  const [queryClient] = useState(() => new QueryClient())
+  const [trpcClient] = useState(() =>
+    trpc.createClient({
+      links: [
+        ipcLink(),
+        loggerLink({
+          // Use whatever condition you want here
+          enabled: () => true
+        })
+      ],
+      transformer: SuperJSON
+    })
+  )
   return (
-    <>
-      <img alt="logo" className="logo" src={electronLogo} />
-      <div className="creator">Powered by electron-vite</div>
-      <div className="text">
-        Build an Electron app with <span className="react">React</span>
-        &nbsp;and <span className="ts">TypeScript</span>
-      </div>
-      <p className="tip">
-        Please try pressing <code>F12</code> to open the devTool
-      </p>
-      <div className="actions">
-        <div className="action">
-          <a href="https://electron-vite.org/" target="_blank" rel="noreferrer">
-            Documentation
-          </a>
-        </div>
-        <div className="action">
-          <a target="_blank" rel="noreferrer" onClick={ipcHandle}>
-            Send IPC
-          </a>
-        </div>
-      </div>
-      <Versions></Versions>
-    </>
+    <trpc.Provider client={trpcClient} queryClient={queryClient}>
+      <QueryClientProvider client={queryClient}>
+        <Home />
+      </QueryClientProvider>
+    </trpc.Provider>
   )
 }
-
-export default App
