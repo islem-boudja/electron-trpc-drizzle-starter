7dad6adb75b33dcccedaf00142b192d3668d73c3
islem-boudja <i_boudja@estin.dz>
2024-07-18 23:46:10 +0100

    refactor: Update Home component to use Tailwind CSS for styling
    This commit updates the Home component in the src/renderer/src/Home.tsx file to use Tailwind CSS for styling. The div element now has the "bg-slate-600" class applied, which sets the background color to slate. This change improves the visual appearance of the Home component by leveraging Tailwind CSS classes.

--- src/renderer/src/Home.tsx
+++ src/renderer/src/Home.tsx
@@ -3,7 +3,7 @@ import { trpc } from './util'
 const Home = () => {
   const { data } = trpc.greting.hello.useQuery()
   if (!data) return <div>Loading...</div>
-  return <div>{data}</div>
+  return <div className=" bg-slate-600">{data}</div>
 }
 
 export default Home
