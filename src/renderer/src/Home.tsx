50f07368fd42f129a8a38835d145629c637868a6
islem-boudja <i_boudja@estin.dz>
2024-07-20 14:59:42 +0100

    refactor: Update Home component to add user creation functionality
    This commit updates the Home component in the src/renderer/src/Home.tsx file to include the functionality for creating users. It adds a button that triggers the handleAddUser function, which makes an API call to create a new user. After the user is created, the getUsers query is invalidated to fetch the updated list of users. This change enhances the Home component by allowing users to be added dynamically.

--- src/renderer/src/Home.tsx
+++ src/renderer/src/Home.tsx
@@ -2,8 +2,24 @@ import { trpc } from './util'
 
 const Home = () => {
   const { data } = trpc.greting.hello.useQuery()
+  const userMutate = trpc.greting.createUser.useMutation()
+  const users = trpc.greting.getUsers.useQuery()
+  const utils = trpc.useUtils()
+  const handleAddUser = async () => {
+    const result = await userMutate.mutateAsync()
+    await utils.greting.getUsers.invalidate()
+    console.log(result)
+  }
   if (!data) return <div>Loading...</div>
-  return <div className=" bg-slate-600">{data}</div>
+  return (
+    <div className=" bg-slate-600">
+      <h1>{data}</h1>
+      <div>
+        <button onClick={handleAddUser}>add user</button>
+        <div>{users.data?.map((user) => <div key={user.id}>{user.textModifiers}</div>)}</div>
+      </div>
+    </div>
+  )
 }
 
 export default Home
