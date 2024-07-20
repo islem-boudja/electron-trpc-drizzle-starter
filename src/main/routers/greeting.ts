18632130358b41ec7a676351f6a53e5afb798522
islem-boudja <i_boudja@estin.dz>
2024-07-20 14:59:54 +0100

    refactor: Add getUsers and createUser queries to greetingRouter
    This commit adds the getUsers and createUser queries to the greetingRouter in the src/main/routers/greeting.ts file. The getUsers query fetches the list of users from the database using the db.select() method, while the createUser query inserts a new user into the database using the db.insert() method. These changes enhance the functionality of the greetingRouter by allowing users to be fetched and created.

--- src/main/routers/greeting.ts
+++ src/main/routers/greeting.ts
@@ -1,7 +1,23 @@
 import { router, publicProcedure } from '../trpc'
+import { db } from '../drizzle'
+import { users } from '../db/schema'
 
 export const greetingRouter = router({
   hello: publicProcedure.query(() => {
     return 'Hello from trpc '
+  }),
+  getUsers: publicProcedure.query(async () => {
+    const result = await db.select().from(users)
+    return result
+  }),
+  createUser: publicProcedure.mutation(async () => {
+    const result = await db
+      .insert(users)
+      .values({
+        id: '1',
+        textModifiers: 'some value'
+      })
+      .returning()
+    return result
   })
 })
