466f1220816fb2a051820a9fbb98e74ec65409c7
islem-boudja <i_boudja@estin.dz>
2024-07-21 17:57:17 +0100

    chore: Update drizzle.config.ts to use VITE_DATABASE_URL environment variable
    This commit updates the drizzle.config.ts file to use the VITE_DATABASE_URL environment variable instead of the DATABASE_URL environment variable. This change enhances the configuration by aligning it with the updated environment variable naming convention.

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
   out: './drizzle',
   dialect: 'postgresql',
   dbCredentials: {
-    url: process.env.DATABASE_URL || ''
+    url: process.env.VITE_DATABASE_URL || ''
   },
   verbose: true,
   strict: true
