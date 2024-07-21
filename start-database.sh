7679cad0223264097280404a491616c90736254e
islem-boudja <i_boudja@estin.dz>
2024-07-21 17:57:05 +0100

    fix: Update POSTGRES_DB value in start-database.sh script
    This commit updates the POSTGRES_DB environment variable in the start-database.sh script. The new value is "electron-trpc-drizzle". This change enhances the script by using a more descriptive and accurate database name.

--- start-database.sh
+++ start-database.sh
@@ -50,6 +50,6 @@ docker run -d \
   --name $DB_CONTAINER_NAME \
   -e POSTGRES_USER="postgres" \
   -e POSTGRES_PASSWORD="$DB_PASSWORD" \
-  -e POSTGRES_DB=drizzle-t3 \
+  -e POSTGRES_DB=electron-trpc-drizzle \
   -p "$DB_PORT":5432 \
   docker.io/postgres && echo "Database container '$DB_CONTAINER_NAME' was successfully created"
